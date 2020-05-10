c8c4bdc9cdcf80360475b7ef258902031d671916
tom <tom-sabatier@hotmail.fr>
2020-05-10 17:50:22 +0200

    modif titre

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Aws monstre
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    title: 'Aws monstre'
+  }
+
+  handleChange = (event) => {
+    const title = event.target.value
+    this.setState({
+      title
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <p>
+            {this.state.title}
+          </p>
+  
+          <input onChange={this.handleChange} type="text" placeholder='modif' />
+
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
