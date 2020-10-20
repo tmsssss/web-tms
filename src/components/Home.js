cbe5b3e6aa26e7ba3ee9901a109b58cb61ed65ae
Tom Sabatier <tom-sabatier@hotmail.fr>
2020-10-20 12:44:38 +0200

    svg about

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,43 +1,105 @@
-import { withNamespaces } from 'react-i18next';
+import { withNamespaces } from "react-i18next";
 
-import React from 'react'
+
+import React from "react";
 
 export const Home = ({ t }) => {
-    return (
-        <section className="mh-home" id="tab-home">
-            <div className="home-ovimg">
-                <div className="container">
-                    <div className="row xs-column-reverse section-separator vertical-middle-content home-padding">
-                        <div className="col-sm-6">
-                            <div className="mh-header-info">
-                                <div className="mh-promo wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.1s">
-                                    <span role="img" aria-label="spoke">🖖</span>
-                                </div>
-                                <h2 className="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s">Tom Sabatier</h2>
-                                <h1 itemScope className="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.3s">{t('jobTitle')}</h1>       
-                                <ul>
-                                    <li className="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s"><i className="fa fa-envelope"></i><a href="mailto:tom.sbtr9@gmail.com">tom.sbtr9@gmail.com</a></li>
-                                    <li className="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.5s"><i className="fa fa-phone"></i>+33 6 38 60 65 57</li>
-                                    <li className="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.6s"><i className="fa fa-map-marker"></i><address>Montpellier</address></li>
-                                </ul>
-                                
-                                <ul className="social-icon wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.7s">
-                                    <li><a target="_blank" rel="noopener noreferrer" href="https://github.com/tmsssss"><i className="fa fa-github"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col-sm-6">
-                            <div className="hero-img wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.6s">
-                                <div className="img-border">
-                                    <img src="tom.png" alt="" width='425' height='425' className="img-fluid"/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <section className="mh-home" id="tab-home">
+      <div className="home-ovimg">
+        <div className="container">
+          <div className="row xs-column-reverse section-separator vertical-middle-content home-padding">
+            <div className="col-sm-6">
+              <div className="mh-header-info">
+                <div
+                  className="mh-promo wow fadeInUp"
+                  data-wow-duration="0.8s"
+                  data-wow-delay="0.1s"
+                >
+                  <span role="img" aria-label="spoke">
+                    🖖
+                  </span>
                 </div>
-            </div>
-        </section> 
-    )
-}
+                <h2
+                  className="wow fadeInUp"
+                  data-wow-duration="0.8s"
+                  data-wow-delay="0.2s"
+                >
+                  Tom Sabatier
+                </h2>
+                <h1
+                  itemScope
+                  className="wow fadeInUp"
+                  data-wow-duration="0.8s"
+                  data-wow-delay="0.3s"
+                >
+                  {t("jobTitle")}
+                </h1>
+                <ul>
+                  <li
+                    className="wow fadeInUp"
+                    data-wow-duration="0.8s"
+                    data-wow-delay="0.4s"
+                  >
+                    <i className="fa fa-envelope"></i>
+                    <a href="mailto:tom.sbtr9@gmail.com">tom.sbtr9@gmail.com</a>
+                  </li>
+                  <li
+                    className="wow fadeInUp"
+                    data-wow-duration="0.8s"
+                    data-wow-delay="0.5s"
+                  >
+                    <i className="fa fa-phone"></i>+33 6 38 60 65 57
+                  </li>
+                  <li
+                    className="wow fadeInUp"
+                    data-wow-duration="0.8s"
+                    data-wow-delay="0.6s"
+                  >
+                    <i className="fa fa-map-marker"></i>
+                    <address>Montpellier</address>
+                  </li>
+                </ul>
 
-export default withNamespaces()(Home)
+                <ul
+                  className="social-icon wow fadeInUp"
+                  data-wow-duration="0.8s"
+                  data-wow-delay="0.7s"
+                >
+                  <li>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://github.com/tmsssss"
+                    >
+                      <i className="fa fa-github"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <div
+                className="hero-img wow fadeInUp"
+                data-wow-duration="0.8s"
+                data-wow-delay="0.6s"
+              >
+                <div className="img-border">
+                  <img
+                    src="tom.png"
+                    alt=""
+                    width="425"
+                    height="425"
+                    className="img-fluid"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default withNamespaces()(Home);
